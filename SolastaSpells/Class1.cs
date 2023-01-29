07e4a1268697881d468505249bcef43c25602b1d
Pikachar2 <highiq2@hotmail.com>
2023-01-29 15:02:41 -0700

    added .gitignore

---
+++ SolastaSpells/Class1.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityModManagerNet;
+
+namespace SolastaSpells
+{
+    static class Main
+    {
+        // Simply call. Compiled without dependencies on UnityModManagerNet
+        static void Load()
+        {
+            // Something
+        }
+
+        // Transfer a variable with data about the mod.
+        //static void Load(UnityModManager.ModEntry modEntry)
+        //{
+        //    // Something
+        //}
+
+        //// Send a response to the mod manager about the launch status, success or not.
+        //static bool Load(UnityModManager.ModEntry modEntry)
+        //{
+        //    // Something
+        //    return true; // If false the mod will show an error.
+        //}
+    }
+}
